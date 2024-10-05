b22004ce977e5822b56e715be01a4466f9df6d3a
Bisma <bismaparveen87@gmail.com>
2024-10-06 00:03:07 +0530

    first commit

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Background from "../Images/bck.jpg"
+
+
+export default function Login() {
+  return (
+   <div className="login">
+    <div className="container">
+        <div className="bg">
+<img src={Background} alt="Background" />
+        </div>
+    </div>
+   </div>
+  )
+}
